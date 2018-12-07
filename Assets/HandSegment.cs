220ba254aa098b9b44764219e46f10b239c8f5a7
Matt Whitlock <matthew.whitlock@colorado.edu>
2018-12-07 11:04:33 -0700

    Initial commit

---
+++ Assets/HandSegment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandSegment : MonoBehaviour {
+    public string startPoint;
+    public string endPoint;
+
+    private GameObject startObject;
+    private GameObject endObject;
+	// Use this for initialization
+	void Start () {
+        startObject = GameObject.Find("Point" + startPoint);
+        endObject = GameObject.Find("Point" + endPoint);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 pos = Vector3.Lerp(startObject.transform.position, endObject.transform.position, (float)0.5);
+        transform.localScale = new Vector3(0.025f, Vector3.Distance(startObject.transform.position, endObject.transform.position) / 2, 0.025f) / transform.parent.localScale.x;
+        transform.position = pos;
+        transform.up = startObject.transform.position - endObject.transform.position;
+    }
+}
---
+++ Assets/LoadGesture.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Globalization;
+
+public class LoadGesture : MonoBehaviour {
+    public string datasetPath;
+    public string gestureNum;
+    private List<GameObject> jointPoints;
+
+    private string subjectNum;
+    private string essaiNum;
+
+    private StreamReader reader;
+
+	// Use this for initialization
+	void Start ()
+    {
+        jointPoints = new List<GameObject>();
+		foreach (Transform child in transform)
+        {
+            jointPoints.Add(child.gameObject);
+        }
+
+        System.Random rnd = new System.Random();
+        subjectNum = rnd.Next(1, 21).ToString();
+        essaiNum = rnd.Next(1, 6).ToString();
+
+        reader = new StreamReader(datasetPath + "gesture_" + gestureNum + "\\finger_1\\subject_" + subjectNum + "\\essai_" + essaiNum + "\\skeleton_world.txt");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        string line;
+        if((line = reader.ReadLine()) != null)
+        {
+            string[] rawCoords = line.Split(' ');
+            for (int i = 0; i < 22; i++)
+            {
+                jointPoints[i].transform.localPosition = new Vector3(float.Parse(rawCoords[3 * i], CultureInfo.InvariantCulture.NumberFormat),
+                    float.Parse(rawCoords[3 * i + 1], CultureInfo.InvariantCulture.NumberFormat),
+                    float.Parse(rawCoords[3 * i + 2], CultureInfo.InvariantCulture.NumberFormat));
+            }
+
+        }
+        else
+        {
+            reader = new StreamReader(datasetPath + "gesture_" + gestureNum + "\\finger_1\\subject_" + subjectNum + "\\essai_" + essaiNum + "\\skeleton_world.txt");
+        }
+    }
+}
